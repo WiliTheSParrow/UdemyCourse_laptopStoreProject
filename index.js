b7f7d215b5557e4a934c118f7a26469ee5193fd0
Júlia W <w.julia91@gmail.com>
2020-07-28 16:15:47 +0200

    cards out

--- index.js
+++ index.js
@@ -13,32 +13,42 @@ const server = http.createServer((req, res) => {
     //console.log(url.parse(req.url, true));
     // url format: http://127.0.0.1:1337/laptop?id=4&name=apple&date=today
 
+
+    //PRODUCT OVERVIEW
     if (pathName === '/products' || pathName === '/') {
         res.writeHead(200, {
             'Content-type': 'text/html'
         });
-        res.end('This is the PRODUCTS page!');
-    } else if (pathName === '/laptop' && id < laptopData.length) {
+
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                // All the objects are stored here:
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}',cardsOutput);
+                // we send the data here
+                res.end(overviewOutput);
+
+            });
+        });
+    }
+
+    // LAPTOP DETAIL
+    else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {
             'Content-type': 'text/html'
         });
 
         fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
             const laptop = laptopData[id];
-            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-            // ! We have to do it from the output bef for the other ones.
-            output = output.replace(/{%IMAGE%}/g, laptop.image);
-            output = output.replace(/{%PRICE%}/g, laptop.price);
-            output = output.replace(/{%SCREEN%}/g, laptop.screen);
-            output = output.replace(/{%CPU%}/g, laptop.cpu);
-            output = output.replace(/{%STORAGE%}/g, laptop.storage);
-            output = output.replace(/{%RAM%}/g, laptop.ram);
-            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            const output = replaceTemplate(data, laptop);
             // sending data to the page
             res.end(output);
         });
 
-    } else {
+    }
+    // URL NOT FOUND
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html'
         });
@@ -51,3 +61,18 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for request now');
 })
+
+function replaceTemplate(originalHTML, laptop) {
+    let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    // ! We have to do it from the output bef for the other ones.
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+
+}
